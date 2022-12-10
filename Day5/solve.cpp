cc3d6bc0083145e7f9dd916fb17b6edc9e931d70
lgrodrigues51101 <liviorodrigues9@gmail.com>
2022-12-10 18:32:16 +0000

    Day5 refactor

--- Day5/solve.cpp
+++ Day5/solve.cpp
@@ -2,9 +2,26 @@
 #include <fstream>
 #include <string>
 #include <stack>
+#include <vector>
 using namespace std;
 
 int MAX_STACK_SIZE = 9;
+
+vector<string> split(string str, string delimiter)
+{
+    vector<string> res;
+    size_t pos = str.find(delimiter);
+
+    while (pos != string::npos)
+    {
+        string token = str.substr(0, pos);
+        res.push_back(token);
+        str.erase(0, pos + delimiter.length());
+        pos = str.find(delimiter);
+    }
+    res.push_back(str);
+    return res;
+}
 
 void print_stack(stack<char> stack)
 {
@@ -17,19 +34,16 @@ void print_stack(stack<char> stack)
 
 int main()
 {
-
     ifstream file("input");
     string line;
 
     stack<char> r_stack[MAX_STACK_SIZE];
     stack<char> stacks[MAX_STACK_SIZE];
-    bool parse_stack = true;
 
     while (getline(file, line))
     {
-        if (parse_stack && line.find("[") == string::npos)
+        if (line.find("[") == string::npos)
         {
-            parse_stack = false;
             getline(file, line);
             // invert stacks
             for (size_t i = 0; i < MAX_STACK_SIZE; i++)
@@ -41,67 +55,47 @@ int main()
                 }
             }
             cout << "finished parsing stack" << endl;
-            continue;
+            break;
         }
 
-        if (parse_stack)
+        int stack_index = 0;
+        for (size_t i = 0; i < line.length(); i = i + 4)
         {
-            int stack_index = 0;
-            for (size_t i = 0; i < line.length(); i = i + 4)
-            {
-                string token = line.substr(i, 3);
+            string token = line.substr(i, 3);
 
-                if (token.find("[") != string::npos)
-                {
-                    r_stack[stack_index].push(token[1]);
-                }
-                stack_index++;
+            if (token.find("[") != string::npos)
+            {
+                r_stack[stack_index].push(token[1]);
             }
+            stack_index++;
         }
-        else
+    }
+
+    while (getline(file, line))
+    {
+        int reps, from, dest;
+
+        string token1;
+        int pos = 0;
+        string delim = " ";
+        int index = 0;
+
+        vector<string> substr = split(line, " ");
+        reps = stoi(substr[1]);
+        from = stoi(substr[3]);
+        dest = stoi(substr[5]);
+
+        stack<char> aux;
+        for (size_t i = 0; i < reps; i++)
         {
-            int reps, from, dest;
+            aux.push(stacks[from - 1].top());
+            stacks[from - 1].pop();
+        }
 
-            string token1;
-            int pos = 0;
-            string delim = " ";
-            int index = 0;
-            while ((pos = line.find(delim)) != std::string::npos)
-            {
-                token1 = line.substr(0, pos); // store the substring
-                // cout << token1 << endl;
-                line.erase(0, pos + delim.length()); /* erase() function store the current positon and move to next token. */
-
-                if (index == 1)
-                {
-                    // cout << "reps " << token1 << endl;
-                    reps = stoi(token1);
-                }
-                else if (index == 3)
-                {
-                    // cout << "from " << token1 << endl;
-                    from = stoi(token1);
-                }
-                index++;
-            }
-
-            // cout << "dest " << line << endl;
-            dest = stoi(line);
-            // cout << "Final " << reps << " " << from << " " << dest << " " << endl;
-
-            stack<char> aux;
-            for (size_t i = 0; i < reps; i++)
-            {
-                aux.push(stacks[from-1].top());
-                stacks[from-1].pop();
-            }
-
-            for (size_t i = 0; i < reps; i++)
-            {
-                stacks[dest-1].push(aux.top());
-                aux.pop();
-            }
-            
+        for (size_t i = 0; i < reps; i++)
+        {
+            stacks[dest - 1].push(aux.top());
+            aux.pop();
         }
     }
 
@@ -110,11 +104,4 @@ int main()
         cout << stacks[i].top();
     }
     cout << endl;
-
-    // print_stack(stack[0]);
-    // // cout << endl;
-    // print_stack(stack[1]);
-    // // cout << endl;
-    // print_stack(stack[2]);
-    // cout << endl;
 }
