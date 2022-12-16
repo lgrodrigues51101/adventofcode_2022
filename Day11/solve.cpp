f2e1ed237577443687badde308764ce32e726cfc
lgrodrigues51101 <liviorodrigues9@gmail.com>
2022-12-16 16:24:29 +0000

    Day 11

--- Day11/solve.cpp
+++ Day11/solve.cpp
@@ -5,20 +5,36 @@
 #include <queue>
 using namespace std;
 
-int max_round = 20;
+int max_round = 10000;
+
+class Item
+{
+public:
+    vector<size_t> values;
+    vector<size_t> div;
+    Item(size_t n_monkeys, size_t value, vector<size_t> Idiv)
+    {
+        div = Idiv;
+        for (size_t i = 0; i < n_monkeys; i++)
+        {
+            values.push_back(value);
+        }
+    }
+};
 
 class monkey
 {
 public:
     int id;
-    queue<int> items;
-    int test_n;
+    queue<Item> items_s;
+    vector<size_t> items; // these are only the starting values
+    size_t test_n;
     int times_inspected;
     string op;
     string op_n;
     int true_monkey;
     int false_monkey;
-    monkey(int iid, int itest_n, string iop, string iop_n, int itru_monkey, int ifalse_monkey, queue<int> iitems)
+    monkey(int iid, int itest_n, string iop, string iop_n, int itru_monkey, int ifalse_monkey, vector<size_t> iitems)
     {
         id = iid;
         test_n = itest_n;
@@ -31,46 +47,52 @@ public:
     }
     void give_item(int item)
     {
-        items.push(item);
+        items.push_back(item);
+    }
+    void give_item(Item item)
+    {
+        items_s.push(item);
     }
     void inspect(vector<monkey> *monkeys)
     {
-        while (!items.empty())
+        while (!items_s.empty())
         {
-            int item = items.front();
-            items.pop();
+            Item item = items_s.front();
+            items_s.pop();
 
-            int op_value;
-            if (op_n == "old")
+            for (size_t i = 0; i < item.values.size(); i++)
             {
-                op_value = item;
-            }
-            else
-            {
-                op_value = stoi(op_n);
-            }
+                size_t op_value;
+                if (op_n == "old")
+                {
+                    op_value = item.values[i];
+                }
+                else
+                {
+                    op_value = stoi(op_n);
+                }
 
-            if (op == "*")
-            {
-                item = item * op_value;
+                if (op == "*")
+                {
+                    item.values[i] = (item.values[i] * op_value) % item.div[i];
+                }
+                else if (op == "/")
+                {
+                    item.values[i] = (item.values[i] / op_value) % item.div[i];
+                }
+                else if (op == "+")
+                {
+                    item.values[i] = (item.values[i] + op_value) % item.div[i];
+                }
+                else if (op == "-")
+                {
+                    item.values[i] = (item.values[i] - op_value) % item.div[i];
+                }
             }
-            else if (op == "/")
-            {
-                item = item / op_value;
-            }
-            else if (op == "+")
-            {
-                item = item + op_value;
-            }
-            else if (op == "-")
-            {
-                item = item - op_value;
-            }
-
-            item = item / 3;
             times_inspected++;
 
-            int index = item % test_n == 0 ? true_monkey : false_monkey;
+            int index = item.values[id] % test_n == 0 ? true_monkey : false_monkey;
+
             (*monkeys)[index].give_item(item);
         }
     }
@@ -108,6 +130,7 @@ int main()
     string line;
 
     vector<monkey> monkeys;
+    vector<size_t> test_ns;
 
     while (getline(file, line)) // monkey id
     {
@@ -118,10 +141,10 @@ int main()
         getline(file, line); // monkey items
         token = split(line, ":");
         token = split(token[1], ",");
-        queue<int> items;
+        vector<size_t> items;
         for (size_t i = 0; i < token.size(); i++)
         {
-            items.push(stoi(token[i]));
+            items.push_back(stoi(token[i]));
         }
 
         getline(file, line); // monkey operation
@@ -132,6 +155,8 @@ int main()
         getline(file, line); // monkey Test
         token = split(line, " ");
         int test_n = stoi(token[5]);
+
+        test_ns.push_back(test_n);
 
         getline(file, line); // true
         token = split(line, " ");
@@ -145,22 +170,32 @@ int main()
         getline(file, line);
     }
 
+    for (size_t i = 0; i < monkeys.size(); i++)
+    {
+        for (size_t j = 0; j < monkeys[i].items.size(); j++)
+        {
+            Item it = Item(monkeys.size(), monkeys[i].items[j], test_ns);
+            monkeys[i].give_item(it);
+        }
+    }
+
     // for (size_t i = 0; i < monkeys.size(); i++)
     // {
     //     cout << monkeys[i].toString() << endl;
     // }
 
-    for (size_t i = 0; i < max_round; i++)
+    for (size_t i = 1; i <= max_round; i++)
     {
         for (size_t j = 0; j < monkeys.size(); j++)
         {
             monkeys[j].inspect(&monkeys);
         }
+
         // cout << "Round " << i << endl;
         // for (size_t j = 0; j < monkeys.size(); j++)
         // {
         //     cout << "monkey " << j << ": ";
-        //     queue<int> Q = monkeys[j].items;
+        //     queue<size_t> Q = monkeys[j].items;
         //     while (!Q.empty())
         //     {
         //         cout << " " << Q.front();
@@ -168,21 +203,32 @@ int main()
         //     }
         //     cout << endl;
         // }
+
+        if (i % 1000 == 0 || i == 1 || i == 20)
+        {
+            cout << "Round " << i << endl;
+            for (size_t j = 0; j < monkeys.size(); j++)
+            {
+                cout << "Monkey " << to_string(j) << " inspected items " << monkeys[j].times_inspected << " times" << endl;
+            }
+        }
     }
 
-    int monkey_business;
-    int best_2[2] = {0,0};
+    size_t monkey_business;
+    size_t best_2[2] = {0, 0};
 
     for (size_t j = 0; j < monkeys.size(); j++)
     {
-        cout << "Monkey " << to_string(j) << " inspected items " << monkeys[j].times_inspected << " times" << endl;
+        // cout << "Monkey " << to_string(j) << " inspected items " << monkeys[j].times_inspected << " times" << endl;
         if (monkeys[j].times_inspected > best_2[0])
         {
             best_2[1] = best_2[0];
             best_2[0] = monkeys[j].times_inspected;
-        } else if(monkeys[j].times_inspected > best_2[1]){
+        }
+        else if (monkeys[j].times_inspected > best_2[1])
+        {
             best_2[1] = monkeys[j].times_inspected;
-        }       
+        }
     }
     cout << best_2[0] << " " << best_2[1] << endl;
     monkey_business = best_2[0] * best_2[1];
